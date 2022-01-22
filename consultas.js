0b22ff1879fd866643785038d3665f20a4f5b02f
heyliceeee <alicedias2002@hotmail.com>
2022-01-22 19:20:03 +0000

    consultas

--- consultas.js
+++ consultas.js
@@ -26,8 +26,58 @@ docs.forEach(function (doc) {
 
 
 -------------------------------------------------------------------------------------------------------------------------
-    Converter os campos "capacidade" string -> int dentro de array
+    Converter o object "proposta" em array
 -------------------------------------------------------------------------------------------------------------------------
 
+var docs = db.edificios.aggregate([
+    { $match: { "proposta": { $exists: true } } },
+    { $addFields: { proposta: ["$proposta"] } }
+])
+
+docs.forEach(function (doc) {
+    db.edificios.updateOne({ _id: doc._id }, { $set: { proposta: doc.proposta } })
+})
+
+
+
+-------------------------------------------------------------------------------------------------------------------------
+    Converter o campo "capacidade" string -> int dentro de array
+-------------------------------------------------------------------------------------------------------------------------
+
+var docs = db.edificios.aggregate([
+    { $unwind: "$facilidade" },
+    { $addFields: { "facilidade.capacidade": { $toInt: "$facilidade.capacidade" } } },
+    { $group: { _id: "$_id", facilidade: { $push: "$facilidade" } } }
+])
+
+docs.forEach(function (doc) {
+    db.edificios.updateOne({ _id: doc._id }, { $set: { facilidade: doc.facilidade } })
+})
+
+
+
+-------------------------------------------------------------------------------------------------------------------------
+    Converter o campo "custo" string -> int dentro de array
+-------------------------------------------------------------------------------------------------------------------------
+
+var docs = db.edificios.aggregate([
+    { $unwind: "$proposta" },
+    { $addFields: { "proposta.custo": { $toInt: "$proposta.custo" } } },
+    { $group: { _id: "$_id", proposta: { $push: "$proposta" } } }
+])
+
+docs.forEach(function (doc) {
+    db.edificios.updateOne({ _id: doc._id }, { $set: { proposta: doc.proposta } })
+})
+
+
+
+-------------------------------------------------------------------------------------------------------------------------
+    Converter o array "proposta" em object
+-------------------------------------------------------------------------------------------------------------------------
+
+    db.edificios.updateMany({}, [
+        { '$addFields': { 'proposta': { '$arrayElemAt': ['$proposta', 0] } } }
+    ])
 
 
